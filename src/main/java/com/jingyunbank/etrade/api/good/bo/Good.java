ad3d4eaaae3edf09bb55f153383082cce1fda37b
liug <liug@liug-PC>
2015-11-03 15:22:09 +0800

    商品管理接口类_liug_20151103

---
+++ src/main/java/com/jingyunbank/etrade/api/good/bo/Good.java
@@ -0,0 +1,14 @@
+package com.jingyunbank.etrade.api.good.bo;
+
+import java.io.Serializable;
+/**
+ * 
+ * @author liug
+ *
+ */
+public class Good  implements Serializable{
+
+	private static final long serialVersionUID = 2563253149671693408L;
+	 
+	
+}
---
+++ src/main/java/com/jingyunbank/etrade/api/good/service/IGoodService.java
@@ -0,0 +1,10 @@
+package com.jingyunbank.etrade.api.good.service;
+/**
+ * 
+ * @author liug
+ *
+ */
+public interface IGoodService {
+
+	 
+}
