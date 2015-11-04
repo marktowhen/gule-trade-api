681f0d6a71588afb89deaa07decebf2890e71d7c
dxf <dxf@dxf-PC>
2015-11-04 14:14:33 +0800

    商品api

--- src/main/java/com/jingyunbank/etrade/api/goods/bo/Goods.java
+++ src/main/java/com/jingyunbank/etrade/api/goods/bo/Goods.java
@@ -1,14 +1,14 @@
 package com.jingyunbank.etrade.api.goods.bo;
 
 import java.io.Serializable;
+
 /**
  * 
  * @author liug
  *
  */
-public class Goods  implements Serializable{
+public class Goods extends BaseGoods implements Serializable {
 
 	private static final long serialVersionUID = 2563253149671693408L;
-	 
-	
+
 }
