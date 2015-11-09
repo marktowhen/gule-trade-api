7cdb4a4fbe3a1ea3c214c7c720ff49ab9dbfdac4
liug <liug@liug-PC>
2015-11-09 17:17:24 +0800

    liug_20151109

--- src/main/java/com/jingyunbank/etrade/api/goods/bo/HotGoods.java
+++ src/main/java/com/jingyunbank/etrade/api/goods/bo/HotGoods.java
@@ -3,11 +3,11 @@ package com.jingyunbank.etrade.api.goods.bo;
 import java.io.Serializable;
 
 /**
- * 
+ * 商品信息业务对象
  * @author liug
  *
  */
-public class Goods extends BaseGoods implements Serializable {
+public class HotGoods extends BaseGoods implements Serializable {
 
 	private static final long serialVersionUID = 2563253149671693408L;
 	
--- src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsService.java
+++ src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.jingyunbank.core.Range;
-import com.jingyunbank.etrade.api.goods.bo.Goods;
+import com.jingyunbank.etrade.api.goods.bo.HotGoods;
 import com.jingyunbank.etrade.api.goods.bo.GoodsShow;
 
 /**
@@ -22,7 +22,7 @@ public interface IGoodsService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listGoodsByLikeName(String goodsname,Range range) throws Exception;
+	public List<HotGoods> listGoodsByLikeName(String goodsname,Range range) throws Exception;
 
 	/**
 	 * 查询所有的品牌
@@ -30,7 +30,7 @@ public interface IGoodsService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listBrands() throws Exception;
+	public List<HotGoods> listBrands() throws Exception;
 
 	/**
 	 * 查询所有的类别
@@ -38,7 +38,7 @@ public interface IGoodsService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listTypes() throws Exception;
+	public List<HotGoods> listTypes() throws Exception;
 
 	/**
 	 * 根据多条件查询商品
@@ -46,7 +46,7 @@ public interface IGoodsService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listGoodsByWhere(GoodsShow goodsshow,Range range) throws Exception;
+	public List<HotGoods> listGoodsByWhere(GoodsShow goodsshow,Range range) throws Exception;
 
 	/**
 	 * 首页查询热门推荐商品
@@ -54,6 +54,6 @@ public interface IGoodsService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listHotGoods() throws Exception;
+	public List<HotGoods> listHotGoods() throws Exception;
 
 }
