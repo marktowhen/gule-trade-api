bfe68610332592a6f5bdbe2596c86abea4896605
dxf <duanxiaofeng@jingyunbank.com>
2015-11-09 15:21:13 +0800

    商品查询service

--- src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsService.java
+++ src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsService.java
@@ -3,47 +3,57 @@ package com.jingyunbank.etrade.api.goods.service;
 import java.util.List;
 import java.util.Map;
 
+import com.jingyunbank.core.Range;
 import com.jingyunbank.etrade.api.goods.bo.Goods;
+import com.jingyunbank.etrade.api.goods.bo.GoodsShow;
 
 /**
  * 
-* Title: 商品接口
-* @author    duanxf
-* @date      2015年11月5日
+ * Title: 商品接口
+ * 
+ * @author duanxf
+ * @date 2015年11月5日
  */
 public interface IGoodsService {
 	/**
 	 * 根据商品名模糊查询商品信息
+	 * 
 	 * @param goodsname
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Goods> listGoodsByLikeName(String goodsname) throws Exception;
+	public List<Goods> listGoodsByLikeName(String goodsname,Range range) throws Exception;
+
 	/**
 	 * 查询所有的品牌
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	public List<Goods> listBrands() throws Exception;
-	
+
 	/**
 	 * 查询所有的类别
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	public List<Goods> listTypes() throws Exception;
+
 	/**
 	 * 根据多条件查询商品
-	 * @param map 参数封装为map对象
+	 * @param goodsshow
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	public List<Goods> listGoodsByWhere(Map<String,Object> map) throws Exception;
+	public List<Goods> listGoodsByWhere(GoodsShow goodsshow,Range range) throws Exception;
+
 	/**
 	 * 首页查询热门推荐商品
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	public List<Goods> listHotGoods() throws Exception;
-	
+
 }
