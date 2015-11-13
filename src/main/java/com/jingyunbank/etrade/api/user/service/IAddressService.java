315925944f4bfc194c17ef0557ab5a377b94bf1e
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2015-11-13 16:03:55 +0800

    收货地址优化

--- src/main/java/com/jingyunbank/etrade/api/user/service/IAddressService.java
+++ src/main/java/com/jingyunbank/etrade/api/user/service/IAddressService.java
@@ -16,7 +16,15 @@ import com.jingyunbank.etrade.api.user.bo.Address;
  */
 public interface IAddressService {
 
-	public boolean save(Address address) throws DataSavingException;
+	/**
+	 * 新增地址 且保证默认地址最多一个
+	 * @param address
+	 * @return
+	 * @throws DataSavingException
+	 * @throws DataRefreshingException
+	 * 2015年11月13日 qxs
+	 */
+	public boolean save(Address address) throws DataSavingException, DataRefreshingException;
 	
 	public boolean refresh(Address address) throws DataRefreshingException;
 	
@@ -63,6 +71,22 @@ public interface IAddressService {
 	 * @throws DataRefreshingException 
 	 */
 	public void refreshDefualt(String id, String uid) throws DataRefreshingException;
+
+	/**
+	 * 查询数量
+	 * @param boFromVo
+	 * @return
+	 * 2015年11月13日 qxs
+	 */
+	public int getAmount(Address boFromVo);
+	
+	/**
+	 * 查询默认收货地址
+	 * @param loginUID
+	 * @return
+	 * 2015年11月13日 qxs
+	 */
+	public Optional<Address> getDefaultAddress(String uid);
 	
 	
 	
