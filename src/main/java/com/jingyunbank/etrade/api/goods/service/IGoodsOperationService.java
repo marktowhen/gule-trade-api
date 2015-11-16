c55d09a08bc0fd052cac00fd5098b5f58ed8bfe1
dxf <duanxiaofeng@jingyunbank.com>
2015-11-16 17:26:05 +0800

    商品操作接口

--- src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsOperationService.java
+++ src/main/java/com/jingyunbank/etrade/api/goods/service/IGoodsOperationService.java
@@ -1,6 +1,13 @@
 package com.jingyunbank.etrade.api.goods.service;
 
+import java.util.Map;
+import java.util.Optional;
+
+import com.jingyunbank.etrade.api.goods.bo.Goods;
+import com.jingyunbank.etrade.api.goods.bo.GoodsDetail;
+import com.jingyunbank.etrade.api.goods.bo.GoodsImg;
 import com.jingyunbank.etrade.api.goods.bo.GoodsOperation;
+import com.jingyunbank.etrade.api.goods.bo.ShowGoods;
 
 /**
  * 
@@ -14,5 +21,46 @@ public interface IGoodsOperationService {
 	 * @param goodsOperation
 	 * @return
 	 */
-	public boolean save(GoodsOperation goodsOperation);
+	public boolean save(GoodsOperation goodsOperation) throws Exception;
+	
+	/**
+	 * 根据商品ID 获取商品详细信息
+	 * @param gid
+	 * @return
+	 * @throws Exception
+	 */
+	public Optional<ShowGoods> singleById(String gid) throws Exception;
+	
+	/**
+	 * 修改商品
+	 * @param goods
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean refreshGoods(GoodsOperation goodsOperation) throws Exception;
+	
+	/**
+	 * 更改商品的库存和销量
+	 * @param gid 商品ID
+	 * @param count 销售数量
+	 * @return true 修改成功  false 修改失败
+	 * @throws Exception
+	 */
+	public boolean refreshGoodsVolume(String gid,int count) throws Exception;
+	
+	/**
+	 * 商品上架
+	 * @param gid
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean refreshGoodsUp(String gid)throws Exception;
+	
+	/**
+	 * 商品下架
+	 * @param gid
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean refreshGoodsDown(String gid)throws Exception;
 }
