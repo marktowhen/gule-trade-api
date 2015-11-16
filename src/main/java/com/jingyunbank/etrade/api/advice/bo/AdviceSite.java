bd4fa5ce7b762e6ca94830568f6e2c319126b17b
guoyuxue <guoyuxue@jingyunbank.com>
2015-11-16 17:01:02 +0800

    资讯模块的提交

---
+++ src/main/java/com/jingyunbank/etrade/api/advice/bo/AdviceSite.java
@@ -0,0 +1,25 @@
+package com.jingyunbank.etrade.api.advice.bo;
+
+public class AdviceSite {
+	private String id;
+	private String siteid;
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSiteid() {
+		return siteid;
+	}
+	public void setSiteid(String siteid) {
+		this.siteid = siteid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
