a3a4cd7f9828fc3a6e8ecae7e74437736c44d09b
guoyuxue <guoyuxue@jingyunbank.com>
2015-11-18 14:32:57 +0800

    评论的优化

--- src/main/java/com/jingyunbank/etrade/api/comment/service/ICommentService.java
+++ src/main/java/com/jingyunbank/etrade/api/comment/service/ICommentService.java
@@ -1,9 +1,11 @@
 package com.jingyunbank.etrade.api.comment.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.jingyunbank.etrade.api.comment.bo.Comments;
-import com.jingyunbank.etrade.api.comment.bo.CommentsImg;
+
+import com.jingyunbank.etrade.api.exception.DataRefreshingException;
 import com.jingyunbank.etrade.api.exception.DataRemovingException;
 import com.jingyunbank.etrade.api.exception.DataSavingException;
 
@@ -14,9 +16,11 @@ public interface ICommentService {
 	//通过gid得到产品的留言
 	public List<Comments> getCommentsByGid(String gid);
 	
-	public Comments getById(String id);
+	public Optional<Comments> getById(String id);
 	
 	public void remove(String id) throws DataRemovingException;
 	
+	public void refreshStatus(Comments comments) throws DataRefreshingException;
 	
+	public void refreshStatus(String[] ids,Comments comments) throws DataRefreshingException;
 }
