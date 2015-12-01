b698b70ab4748ac02643e4537c418d7b7b001408
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2015-12-01 17:39:22 +0800

    积分

---
+++ src/main/java/com/jingyunbank/etrade/api/point/service/IPointLogService.java
@@ -0,0 +1,36 @@
+package com.jingyunbank.etrade.api.point.service;
+
+import java.util.List;
+
+import com.jingyunbank.core.Range;
+import com.jingyunbank.etrade.api.exception.DataSavingException;
+import com.jingyunbank.etrade.api.point.bo.PointLog;
+
+public interface IPointLogService {
+
+	/**
+	 * 列表查询
+	 * @param uid
+	 * @return
+	 * 2015年12月1日 qxs
+	 */
+	public List<PointLog> list(String uid);
+	
+	/**
+	 * 列表查询
+	 * @param uid
+	 * @param range
+	 * @return
+	 * 2015年12月1日 qxs
+	 */
+	public List<PointLog> list(String uid, Range range);
+	
+	/**
+	 * 新增
+	 * @param pointLog
+	 * @return
+	 * @throws DataSavingException
+	 * 2015年12月1日 qxs
+	 */
+	public boolean add(PointLog pointLog) throws DataSavingException;
+}
