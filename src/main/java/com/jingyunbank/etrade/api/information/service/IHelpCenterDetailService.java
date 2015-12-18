7c1df0c29c57bcdf974b59ca14d40392fb9c9c2a
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2015-12-18 13:57:55 +0800

    help center

--- src/main/java/com/jingyunbank/etrade/api/information/service/IHelpCenterDetailService.java
+++ src/main/java/com/jingyunbank/etrade/api/information/service/IHelpCenterDetailService.java
@@ -3,6 +3,7 @@ package com.jingyunbank.etrade.api.information.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.jingyunbank.core.Range;
 import com.jingyunbank.etrade.api.exception.DataRefreshingException;
 import com.jingyunbank.etrade.api.exception.DataRemovingException;
 import com.jingyunbank.etrade.api.exception.DataSavingException;
@@ -25,6 +26,13 @@ public interface IHelpCenterDetailService {
 	 * 2015年12月11日 qxs
 	 */
 	public List<HelpCenterDetail> listAllValid(String categoryID);
+	/**
+	 * 分页查看
+	 * @param range
+	 * @return
+	 * 2015年12月18日 qxs
+	 */
+	public List<HelpCenterDetail> list(Range range);
 	
 	/**
 	 * 新增
@@ -51,5 +59,16 @@ public interface IHelpCenterDetailService {
 	 * 2015年12月10日 qxs
 	 */
 	public boolean remove(String id) throws DataRemovingException;
+	
+	/**
+	 * 删除
+	 * @param categoryID
+	 * @return
+	 * @throws DataRemovingException
+	 * 2015年12月10日 qxs
+	 */
+	public boolean removeByCategory(String categoryID) throws DataRemovingException;
+
+	
 
 }
