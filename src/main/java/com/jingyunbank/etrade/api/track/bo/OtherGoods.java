3cf4f0f69a2779e09e28825de2568c3f59465b70
liuguang <liuguang@jingyunbank.com>
2015-12-25 13:34:14 +0800

    1225

---
+++ src/main/java/com/jingyunbank/etrade/api/track/bo/OtherGoods.java
@@ -0,0 +1,18 @@
+package com.jingyunbank.etrade.api.track.bo;
+
+import java.io.Serializable;
+
+import com.jingyunbank.etrade.api.goods.bo.BaseGoods;
+/**
+ * 其他用户还购买的商品
+ * @author liug
+ *
+ */
+public class OtherGoods extends BaseGoods implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
