b67dc0e6d67ea3e75e8974755306b9fccc36c4b9
Mike Cao <michael-cao@outlook.com>
2016-01-07 15:01:06 +0800

    correction.

--- src/main/java/com/jingyunbank/etrade/api/postage/service/IPostageService.java
+++ src/main/java/com/jingyunbank/etrade/api/postage/service/IPostageService.java
@@ -7,7 +7,15 @@ import com.jingyunbank.etrade.api.order.presale.bo.Orders;
 
 public interface IPostageService {
 
-	public BigDecimal calculate(String mid, BigDecimal orderprice, BigDecimal weight, BigDecimal defaultp);
+	/**
+	 * 计算寄往<code>provenceid</code>省份的价格为<code>orderprice</code>
+	 * 重量为<code>weight</code>的订单的邮费
+	 * @param orderprice
+	 * @param provenceid
+	 * @param weight
+	 * @return
+	 */
+	public BigDecimal calculate(BigDecimal orderprice, int provenceid, BigDecimal weight);
 	
 	public void calculate(List<Orders> orders);
 }
