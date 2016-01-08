d6b9d2253e91cf42a7f42d16b2f4ac98b0318140
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2016-01-08 12:45:07 +0800

    优化脚本

--- docs/changedocs/PROVINCE.sql
+++ docs/changedocs/PROVINCE.sql
@@ -1,4 +1,3 @@
 
 ALTER TABLE province add faraway TINYINT(1) DEFAULT 0;
 
-update province p set p.faraway=1 where p.province_id in ('31','30' ,'26' ,'25' ,'28' ,'5' ,'21' ,'32'  ,'33','34' );
--- docs/dml/初始化ROLE.sql
+++ docs/dml/初始化ROLE.sql
@@ -1,1 +1,2 @@
+delete from role where id='1';
 INSERT INTO `role` VALUES ('1', 'MANAGER', '系统管理员', 1, NULL);
