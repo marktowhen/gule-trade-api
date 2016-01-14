33116dad4cba69b5b6e1ff2fdaa791e3046e00e3
Mike Cao <michael-cao@outlook.com>
2016-01-14 17:31:47 +0800

    CORRECTION of order goods.

--- src/main/java/com/jingyunbank/etrade/api/order/presale/bo/OrderGoods.java
+++ src/main/java/com/jingyunbank/etrade/api/order/presale/bo/OrderGoods.java
@@ -30,7 +30,7 @@ public class OrderGoods implements Serializable{
 	private String MID;
 	private String mname;
 	private String imgpath;
-	private String postage;//用户邮费退换的显示
+	private BigDecimal postage;//用户邮费退换的显示
 	
 	public String getID() {
 		return ID;
@@ -134,10 +134,11 @@ public class OrderGoods implements Serializable{
 	public void setStatusName(String statusName) {
 		this.statusName = statusName;
 	}
-	public String getPostage() {
+	public BigDecimal getPostage() {
 		return postage;
 	}
-	public void setPostage(String postage) {
+	public void setPostage(BigDecimal postage) {
 		this.postage = postage;
 	}
+	
 }
