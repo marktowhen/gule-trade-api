ff4a047c784fe5c42c3198bd9b116beeb61ec934
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2016-04-20 17:24:30 +0800

    团购基础

--- src/main/java/com/jingyunbank/etrade/api/marketing/group/bo/GroupUser.java
+++ src/main/java/com/jingyunbank/etrade/api/marketing/group/bo/GroupUser.java
@@ -11,6 +11,14 @@ public class GroupUser {
 	private Group group;
 	private Date jointime;
 	private BigDecimal paid;
+	private String status;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	public Users getUser() {
 		return user;
 	}
---
+++ src/main/java/com/jingyunbank/etrade/api/marketing/group/service/IGroupUserService.java
@@ -0,0 +1,13 @@
+package com.jingyunbank.etrade.api.marketing.group.service;
+
+import java.util.Optional;
+
+import com.jingyunbank.etrade.api.exception.DataSavingException;
+import com.jingyunbank.etrade.api.marketing.group.bo.GroupUser;
+
+public interface IGroupUserService {
+
+	public boolean save(GroupUser groupUser) throws DataSavingException;
+	
+	public Optional<GroupUser> single(String ID);
+}
