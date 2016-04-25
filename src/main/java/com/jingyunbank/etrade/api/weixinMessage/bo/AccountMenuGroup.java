7ce1ec5ab42e70bac3f229e4e2a1a4a7a6ed1dfd
alex <myfacebookhello@163.com>
2016-04-25 17:20:53 +0800

    微信消息推送代码

---
+++ src/main/java/com/jingyunbank/etrade/api/weixinMessage/bo/AccountMenuGroup.java
@@ -0,0 +1,27 @@
+package com.jingyunbank.etrade.api.weixinMessage.bo;
+
+
+public class AccountMenuGroup extends BaseEntity{
+
+	private String name;
+	private Integer enable;
+
+
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public Integer getEnable(){
+		return enable;
+	}
+	public void setEnable(Integer enable){
+		this.enable = enable;
+	}
+
+
+
+}
+
---
+++ src/main/java/com/jingyunbank/etrade/api/weixinMessage/bo/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.jingyunbank.etrade.api.weixinMessage.bo;
+
+
+import java.util.Date;
+
+public class BaseEntity {
+
+	private Long id;
+	private Date createtime = new Date();//创建时间
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	
+	
+}
